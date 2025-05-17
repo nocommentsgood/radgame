dfaa7cb7792312d7e35b201ca321f457fe7f5331
nocommentsgood <>
2025-05-17 11:45:20 -0500

    squash me too

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,3 +11,13 @@ struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(init,base = Object)]
+struct Dummy;
+
+#[godot_api]
+impl Dummy {
+    #[func]
+    fn p() {}
+}
