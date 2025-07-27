303eb30ef2777755f9d89e3cc96064c62920c6e1
nocommentsgood <boxspring18@gmail.com>
2025-07-27 12:11:14 -0500

    add test navigation enemy

--- rust/src/classes/enemies/nav_enemy.rs
+++ rust/src/classes/enemies/nav_enemy.rs
@@ -8,7 +8,7 @@ use godot::{
 struct NavEnemy {
     #[init(val = 200.0)]
     speed: f32,
-    #[init(val = Vector2::new(-172.0, 260.0))]
+    #[export]
     target_pos: Vector2,
     base: Base<CharacterBody2D>,
 
@@ -21,54 +21,40 @@ impl ICharacterBody2D for NavEnemy {
     fn ready(&mut self) {
         self.nav_agent.set_path_desired_distance(4.0);
         self.nav_agent.set_target_desired_distance(4.0);
+        self.nav_agent
+            .signals()
+            .velocity_computed()
+            .connect_other(&self.to_gd(), Self::on_velocity_computed);
 
-        // let t = self
-        //     .base()
-        //     .get_tree()
-        //     .unwrap()
-        //     .create_timer_ex(1.0)
-        //     .process_always(true)
-        //     .process_in_physics(true)
-        //     .done()
-        //     .unwrap();
-        // t.signals()
-        //     .timeout()
-        //     .connect_other(&self.to_gd(), Self::setup);
-
-        self.base_mut().call_deferred("setup", &[]);
+        self.apply_deferred(|this| this.setup());
     }
 
     fn physics_process(&mut self, _delta: f32) {
         if self.nav_agent.is_navigation_finished() {
-            println!("Finished naving");
             return;
         }
 
-        let cur_pos = self.base().get_global_position();
         let next = self.nav_agent.get_next_path_position();
-        let vel = cur_pos.direction_to(next) * self.speed;
-        self.base_mut().set_velocity(vel);
-        self.base_mut().move_and_slide();
+        let v = self.base().get_global_position().direction_to(next) * self.speed;
+        if self.nav_agent.get_avoidance_enabled() {
+            self.nav_agent.set_velocity(v);
+        }
     }
 }
 
 #[godot_api]
 impl NavEnemy {
-    async fn setup(&mut self) -> godot::task::SignalFuture<()> {
+    fn on_velocity_computed(&mut self, safe_vel: Vector2) {
+        self.base_mut().set_velocity(safe_vel);
+        self.base_mut().move_and_slide();
+    }
+
+    fn setup(&mut self) {
         let tree = self.base().get_tree().unwrap();
-
-        // let p = tree.signals().physics_frame().to_future().await;
-
-        // godot::task::spawn(async move {
-        //     let x: () = Signal::from_object_signal(&tree, "physics_frame")
-        //         .to_future()
-        //         .await;
-        // });
-
-        self.nav_agent.set_target_position(self.target_pos);
-
+        godot::task::spawn(async move {
+            tree.signals().physics_frame().to_future().await;
+        });
         self.set_movement_target(self.target_pos);
-        tree.signals().physics_frame().to_future()
     }
 
     fn set_movement_target(&mut self, target: Vector2) {
