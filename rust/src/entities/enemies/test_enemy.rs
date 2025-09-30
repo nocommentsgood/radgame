0d9718ff976c7bc14928a7685ecb9e33700f713c
nocommentsgood <66213143+nocommentsgood@users.noreply.github.com>
2025-09-30 13:51:20 -0500

    improve enemy type modularity

--- rust/src/entities/enemies/test_enemy.rs
+++ rust/src/entities/enemies/test_enemy.rs
@@ -8,102 +8,107 @@ use super::enemy_state_machine::{EnemyEvent, EnemyStateMachine, State};
 use crate::entities::{
     damage::{AttackData, Damage, DamageType},
     enemies::{
+        enemy_state_machine::EnemySMType,
         has_enemy_sensors::EnemySensors,
-        physics::{self, FrameData, Movement, PatrolComp},
+        physics::{self, Movement, PatrolComp, PhysicsFrameData, Speeds},
         time::{EnemyTimers, Timers},
     },
     ent_graphics::EntGraphics,
     movements::Direction,
 };
 
-#[derive(GodotClass)]
-#[class(base = CharacterBody2D, init)]
-struct ModularTestEnemy {
-    #[export]
-    left_target: Vector2,
-    #[export]
-    right_target: Vector2,
-
-    #[init(val = OnReady::manual())]
-    movement: OnReady<Movement>,
-
-    #[init(val = OnReady::from_base_fn(EntGraphics::new))]
-    graphics: OnReady<EntGraphics>,
-
-    #[init(val = OnReady::from_base_fn(EnemySensors::new))]
-    sensors: OnReady<EnemySensors>,
-
-    #[init(val = OnReady::from_base_fn(Timers::new))]
-    timers: OnReady<Timers>,
-
+#[derive(Clone)]
+struct EnemyEntity {
+    movement: Movement,
+    graphics: EntGraphics,
+    sensors: EnemySensors,
+    timers: Timers,
     patrol: PatrolComp,
-    sm: statig::blocking::StateMachine<EnemyStateMachine>,
-
-    base: Base<CharacterBody2D>,
+    sm: EnemySMType,
 }
 
-#[godot_api]
-impl ICharacterBody2D for ModularTestEnemy {
-    fn ready(&mut self) {
-        self.patrol.right_target = self.right_target;
-        self.patrol.left_target = self.left_target;
-        self.movement.init(physics::Movement::new(
-            physics::Speeds::new(100.0, 150.0),
-            Vector2::default(),
-        ));
-        self.sensors.hit_reg.hurtbox.bind_mut().data = Some(AttackData {
-            hurtbox: self.sensors.hit_reg.hurtbox.clone(),
-            parryable: true,
-            damage: Damage {
-                raw: 10,
-                d_type: DamageType::Physical,
-            },
-        });
+impl EnemyEntity {
+    pub fn new(
+        node: &Gd<Node>,
+        speeds: Speeds,
+        left_patrol_target: Vector2,
+        right_patrol_target: Vector2,
+        sm: EnemySMType,
+    ) -> Self {
+        Self {
+            movement: Movement::new(speeds, Vector2::default()),
+            graphics: EntGraphics::new(node),
+            sensors: EnemySensors::new(node),
+            timers: Timers::new(node),
+            patrol: PatrolComp::new(left_patrol_target, right_patrol_target),
+            sm,
+        }
+    }
 
-        self.sensors
+    pub fn new_and_init(
+        node: &Gd<Node>,
+        speeds: Speeds,
+        left_patrol_target: Vector2,
+        right_patrol_target: Vector2,
+        sm: EnemySMType,
+    ) -> Self {
+        let mut this = Self {
+            movement: Movement::new(speeds, Vector2::default()),
+            graphics: EntGraphics::new(node),
+            sensors: EnemySensors::new(node),
+            timers: Timers::new(node),
+            patrol: PatrolComp::new(left_patrol_target, right_patrol_target),
+            sm,
+        };
+        let mut s = this.clone();
+        this.timers
+            .attack
+            .signals()
+            .timeout()
+            .connect(move || s.on_attack_timeout());
+        let mut s = this.clone();
+        this.timers
+            .idle
+            .signals()
+            .timeout()
+            .connect(move || s.on_idle_timeout());
+        let mut s = this.clone();
+        this.timers
+            .patrol
+            .signals()
+            .timeout()
+            .connect(move || s.on_patrol_timeout());
+        let mut s = this.clone();
+        this.sensors
             .player_detection
             .aggro_area
             .signals()
             .area_entered()
-            .connect_other(&self.to_gd(), Self::on_aggro_area_entered);
-        self.sensors
+            .connect(move |area| s.on_aggro_area_entered(area));
+        let mut s = this.clone();
+        this.sensors
             .player_detection
             .aggro_area
             .signals()
             .area_exited()
-            .connect_other(&self.to_gd(), Self::on_aggro_area_exited);
-        self.sensors
+            .connect(move |area| s.on_aggro_area_exited(area));
+        let mut s = this.clone();
+        this.sensors
             .player_detection
             .attack_area
             .signals()
             .area_entered()
-            .connect_other(&self.to_gd(), Self::on_attack_area_entered);
-
-        self.timers
-            .idle
-            .signals()
-            .timeout()
-            .connect_other(&self.to_gd(), Self::on_idle_timeout);
-        self.timers
-            .patrol
-            .signals()
-            .timeout()
-            .connect_other(&self.to_gd(), Self::on_patrol_timeout);
-        self.timers
-            .attack
-            .signals()
-            .timeout()
-            .connect_other(&self.to_gd(), Self::on_attack_timeout);
-
-        self.timers.idle.start();
+            .connect(move |area| s.on_attack_area_entered(area));
+        dbg!(this.timers.idle.start());
+        this
     }
 
-    fn physics_process(&mut self, delta: f32) {
+    fn physics_process(&mut self, node: &mut Gd<CharacterBody2D>, delta: f32) {
         if self.sensors.is_any_raycast_colliding() {
             self.sm.handle(&EnemyEvent::RayCastNotColliding);
         }
 
-        if !self.base().is_on_floor() {
+        if !node.is_on_floor() {
             self.sm.handle(&EnemyEvent::FailedFloorCheck);
         }
 
@@ -122,17 +127,17 @@ impl ICharacterBody2D for ModularTestEnemy {
                 }
             }
             State::Falling {} => {
-                if self.base().is_on_floor() {
+                if node.is_on_floor() {
                     self.sm.handle(&EnemyEvent::OnFloor);
                 }
             }
             _ => (),
         }
 
-        let frame = FrameData::new(
+        let frame = PhysicsFrameData::new(
             self.sm.state(),
-            self.base().is_on_floor(),
-            self.base().get_global_position(),
+            node.is_on_floor(),
+            node.get_global_position(),
             self.sensors.player_position(),
             &self.patrol,
             delta,
@@ -144,14 +149,16 @@ impl ICharacterBody2D for ModularTestEnemy {
         );
         self.movement.update(&frame);
         let v = self.movement.velocity;
-        self.base_mut().set_velocity(v);
-        self.base_mut().move_and_slide();
+        node.set_velocity(v);
+        node.move_and_slide();
 
-        // dbg!(&self.sm.state());
+        dbg!(&self.sm.state());
     }
+
+    fn process(&mut self, node: &mut Gd<Node2D>, delta: f32) {}
 }
 
-impl ModularTestEnemy {
+impl EnemyEntity {
     fn on_aggro_area_entered(&mut self, _area: Gd<Area2D>) {
         self.sm.handle(&EnemyEvent::FoundPlayer);
     }
@@ -168,17 +175,17 @@ impl ModularTestEnemy {
     }
 
     fn on_idle_timeout(&mut self) {
-        let frame = FrameData::new(
-            self.sm.state(),
-            self.base().is_on_floor(),
-            self.base().get_global_position(),
-            self.sensors.player_position(),
-            &self.patrol,
-            self.base().get_physics_process_delta_time() as f32,
-        );
+        // let frame = FrameData::new(
+        //     self.sm.state(),
+        //     self.base().is_on_floor(),
+        //     self.base().get_global_position(),
+        //     self.sensors.player_position(),
+        //     &self.patrol,
+        //     self.base().get_physics_process_delta_time() as f32,
+        // );
         if self.sm.state() == (&State::Idle {}) {
             self.timers.patrol.start();
-            self.movement.update_patrol_target(&frame);
+            // self.movement.update_patrol_target(&frame);
 
             self.sm.handle(&EnemyEvent::TimerElapsed(EnemyTimers::Idle));
         }
@@ -196,5 +203,28 @@ impl ModularTestEnemy {
     fn on_attack_timeout(&mut self) {
         self.sm
             .handle(&EnemyEvent::TimerElapsed(EnemyTimers::Attack));
+    }
+}
+
+#[derive(GodotClass)]
+#[class(base = CharacterBody2D, init)]
+struct ActualEnemyBody {
+    #[export]
+    left_target: Vector2,
+    #[export]
+    right_target: Vector2,
+
+    #[init(val = OnReady::from_base_fn(|base|
+        EnemyEntity::new_and_init(base, Speeds::new(100.0, 150.0),
+        Vector2::default(), Vector2::default(), EnemySMType::Basic(EnemyStateMachine::new()))))]
+    ent: OnReady<EnemyEntity>,
+    base: Base<CharacterBody2D>,
+}
+
+#[godot_api]
+impl ICharacterBody2D for ActualEnemyBody {
+    fn physics_process(&mut self, delta: f32) {
+        let mut this = self.to_gd().upcast();
+        self.ent.physics_process(&mut this, delta);
     }
 }
